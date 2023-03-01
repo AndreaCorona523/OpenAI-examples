8f97706c6a899164bfc16b3a03d8b74c6fa588a7
AndreaCorona523 <85129680+AndreaCorona523@users.noreply.github.com>
2023-03-01 15:30:30 -0600

    add moderation labels

--- src/app/moderations/moderations.component.ts
+++ src/app/moderations/moderations.component.ts
@@ -11,8 +11,23 @@ export class ModerationsComponent {
   constructor (private moderations: ModerationsService) {}
 
   ngOnInit(): void {}
-  result : string = ""; 
+  
   userPrompt : string = "";
+  result_hate : string = ""; 
+  result_hate_score : string = ""; 
+  result_hate_threatening : string = ""; 
+  result_hate_threatening_score : string = ""; 
+  result_self_harm : string = "";
+  result_self_harm_score : string = "";
+  result_sexual : string = "";
+  result_sexual_score : string = "";
+  result_sexual_minors : string = "";
+  result_sexual_minors_score : string = "";
+  result_violence : string = "";
+  result_violence_score : string = "";
+  result_violence_graphic : string = "";
+  result_violence_graphic_score : string = "";
+  result_flagged : string = "";
 
   postEdit(){
     let myprompt = `${this.userPrompt}`
@@ -24,7 +39,7 @@ export class ModerationsComponent {
     this.moderations.postModeration(payload).subscribe((data: any) =>{
       //alert(JSON.stringify(data));
       console.log(data);
-      this.result = data.results[0].categories;
+      this.result_hate = data.results[0].categories;
       console.log(data.results[0]);
     })
   }
